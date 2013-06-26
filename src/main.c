1184c58d070c1403189be17f5e52b8232d6f1dd3
James Benner <james.benner@gmail.com>
2013-06-25 22:15:07 -0400

    Initial commit.

---
+++ src/main.c
@@ -0,0 +1,23 @@
+#define _GNU_SOURCE // Trigger stdio.h to include asprintf
+#include <stdio.h>
+#include <stdlib.h> // free
+#include <string.h> // strstr, strlen, strtok, size_t, NULL
+
+int main() {
+    char *src = " #include stdio, stdlib, string";
+    char *tok;
+    src = strstr(src, "#include"); // Our psuedo ltrim...
+
+    size_t x = strlen(src);
+    char dest[x];
+    for (size_t i = strlen("#include"), y = 0; y < x; ++i, ++y)
+        dest[y] = src[i];
+    dest[x] = '\0';
+
+    tok = strtok(dest, " ,");
+    while (tok != NULL) {
+        printf("#include <%s.h>\n", tok);
+        tok = strtok(NULL, " ,");
+    }
+    // 0 is implicitely returned on successful completion.
+}
